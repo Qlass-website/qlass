d41c74dc713a619477da55dfcdd084f5ddd935e6
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-03-02 12:52:00 +0530

    🐛 brand image fix in homepage

--- src/components/home-page/brands/Brands.jsx
+++ src/components/home-page/brands/Brands.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import BrandCards from "./BrandCards/BrandCards";
 
 export default function Brands() {
   return (
-    <div>
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/f1ede6dcef613069a97790372bcf22fdd2adf7b12bd02168c940cc890d000aad?"
-        className="aspect-[4.35] w-full"
-      />
+    <div className="flex items-center justify-center">
+      <div className="w-full bg-[#FCFCFC] p-10">
+        <div className="flex flex-col max-md:flex-col-reverse max-md:items-center">
+          <div className="mt-10 text-center max-md:mb-10 max-md:w-full">
+            <div className="mt-4 text-4xl font-medium text-[#50536A]">
+              {" "}
+              Trusted by <span className="text-[#22163F]">1000+</span> Students
+              all over India <br /> Recommended by Govt. of Bihar.
+            </div>
+          </div>
+          <div className="w-full max-md:w-full">
+            <div className="relative min-h-[137px] flex-grow items-center justify-center overflow-hidden max-md:mt-10 max-md:max-w-full">
+              <div className="mt-8">
+                <BrandCards />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
