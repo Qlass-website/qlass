c66e841216a2b0d03ad80355b5a6621846b82e06
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-03-02 12:07:56 +0530

    ✅ Added Speak to out kid section

--- src/components/gallary-page/speak-to-our-kids/SpeakToOurKids.jsx
+++ src/components/gallary-page/speak-to-our-kids/SpeakToOurKids.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import KidsGallary from './KidsGallary/KidsGallary'
 
 export default function SpeakToOurKids() {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-full bg-[#FBF9F4] p-10">
+      <div className="w-full p-10">
         <div className="flex flex-col max-md:flex-col-reverse max-md:items-center">
           <div className="text-center max-md:mb-10 max-md:w-full">
             <div className="mt-4 text-4xl font-bold">
@@ -14,8 +15,8 @@ export default function SpeakToOurKids() {
           </div>
           <div className="w-full max-md:w-full">
             <div className="relative min-h-[340px] flex-grow items-center justify-center overflow-hidden max-md:mt-10 max-md:max-w-full">
-              <div className="mt-8 flex justify-center">
-
+              <div className="mt-12 flex justify-center">
+                <KidsGallary />
               </div>
             </div>
           </div>
