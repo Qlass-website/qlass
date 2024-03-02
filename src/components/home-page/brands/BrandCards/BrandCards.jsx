0c0b075f0e9556c888ecbf792af4c4e445d42345
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-03-02 17:57:17 +0530

    🐛 brand logo fixes

--- src/components/home-page/brands/BrandCards/BrandCards.jsx
+++ src/components/home-page/brands/BrandCards/BrandCards.jsx
@@ -5,28 +5,28 @@ export default function BrandCards() {
     <div className="flex items-center justify-center space-x-32">
       <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[200px]">
         <img
-          src="/Brand/brand1.png"
+          src="/Brand/brandnew1.png"
           className="mx-auto h-auto w-full object-cover"
           alt="Brand 1"
         />
       </div>
       <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[200px]">
         <img
-          src="/Brand/brand2.png"
+          src="/Brand/brandnew2.png"
           className="mx-auto h-auto w-full object-cover"
           alt="Brand 2"
         />
       </div>
       <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[200px]">
         <img
-          src="/Brand/brand3.png"
+          src="/Brand/brandnew3.png"
           className="h-200 mx-auto w-full object-cover"
           alt="Brand 3"
         />
       </div>
       <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[200px]">
         <img
-          src="/Brand/brand4.png"
+          src="/Brand/brandnew4.png"
           className="h-200 mx-auto w-full object-cover"
           alt="Brand 4"
         />
