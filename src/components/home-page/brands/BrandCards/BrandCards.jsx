fc008731ed856647da97121364a89d7d7e1fad50
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-03-03 13:39:19 +0530

    🐛 fixes

--- src/components/home-page/brands/BrandCards/BrandCards.jsx
+++ src/components/home-page/brands/BrandCards/BrandCards.jsx
@@ -17,17 +17,17 @@ export default function BrandCards() {
           alt="Brand 2"
         />
       </div>
-      <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[200px]">
+      <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[150px]">
         <img
           src="/Brand/brandnew3.png"
-          className="h-200 mx-auto w-full object-cover"
+          className="h-auto mx-auto w-full object-cover"
           alt="Brand 3"
         />
       </div>
-      <div className="flex w-full transform flex-col items-center space-y-4 py-6 text-center transition-transform hover:scale-105 md:w-[200px]">
+      <div className="flex w-full transform flex-col items-center space-y-4 text-center transition-transform hover:scale-105 md:w-[120px]">
         <img
           src="/Brand/brandnew4.png"
-          className="h-200 mx-auto w-full object-cover"
+          className="h-auto mx-auto w-full object-cover"
           alt="Brand 4"
         />
       </div>
