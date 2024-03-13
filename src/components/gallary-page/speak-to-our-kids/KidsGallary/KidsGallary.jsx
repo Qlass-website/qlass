52e5c4c4a491a56fd41af3df08703a26b1de101a
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-03-13 21:56:45 +0530

    🐛 gallary fix

--- src/components/gallary-page/speak-to-our-kids/KidsGallary/KidsGallary.jsx
+++ src/components/gallary-page/speak-to-our-kids/KidsGallary/KidsGallary.jsx
@@ -1,175 +1,119 @@
-import ResultsDisplayCards from "@/components/home-page/our-results/ResultsDisplayCards/ResultsDisplayCards";
+import React from "react";
+import {
+  Card,
+  CardDescription,
+  CardTitle
+} from "@/components/ui/card";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardTitle } from "@/components/ui/card";
-import React from "react";
+
+
+const kidsData = [
+  {
+    name: "Shaurya",
+    class: "Class 10",
+    initialPercentage: 47,
+    finalPercentage: 85,
+    duration: "In just 45 Days",
+    image: "/Shaurya.jpg",
+  },
+  {
+    name: "Ujjwal",
+    class: "Class 10",
+    initialPercentage: 40,
+    finalPercentage: 83,
+    duration: "In just 45 Days",
+    image: "https://github.com/shadcn.png",
+  },
+  {
+    name: "Shruti",
+    class: "Class 10",
+    initialPercentage: 25,
+    finalPercentage: 65,
+    duration: "In just 45 Days",
+    image: "/Shruti.jpg",
+  },
+  {
+    name: "Prakhar",
+    class: "Class 10",
+    initialPercentage: 15,
+    finalPercentage: 59,
+    duration: "In just 45 Days",
+    image: "/Prakhar.jpg",
+  },
+  {
+    name: "Pankaj",
+    class: "Class 9",
+    initialPercentage: 60,
+    finalPercentage: 92,
+    duration: "In just 45 Days",
+    image: "https://github.com/shadcn.png",
+  },
+  {
+    name: "Deepasmita",
+    class: "Class 9",
+    initialPercentage: 45,
+    finalPercentage: 88,
+    duration: "In just 45 Days",
+    image: "/Deepsmita.jpg",
+  },
+  {
+    name: "Anshul",
+    class: "Class 8",
+    initialPercentage: 40,
+    finalPercentage: 88,
+    duration: "In just 45 Days",
+    image: "/Anshul.jpg",
+  },
+  {
+    name: "Gunjan",
+    class: "Class 8",
+    initialPercentage: 40,
+    finalPercentage: 84,
+    duration: "In just 45 Days",
+    image: "/Gunjan.jpg",
+  },
+  {
+    name: "Tanmay",
+    class: "Class 8",
+    initialPercentage: 45,
+    finalPercentage: 85,
+    duration: "In just 45 Days",
+    image: "/Tanmay.jpg",
+  },
+];
 
 export default function KidsGallary() {
   return (
-    <div className="mx-auto px-10 md:px-0 max-w-screen-xl">
-      <div className="grid grid-cols-1 gap-x-12 gap-y-4 md:grid-cols-3">
-        <div>
-          <Card className="flex items-center justify-between rounded-xl border-4 border-indigo-800 p-6 md:p-12 w-[350px] h-[200px] md:w-[400px] md:h-[200px]">
-            <CardTitle>
-              <div className="flex items-center">
-                <Avatar className="w-28 h-28">
-                  <AvatarImage src="/Shaurya.jpg" />
+    <div className="mx-auto px-4 md:px-0">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 items-center justify-center">
+        {kidsData.map((kid, index) => (
+          <div key={index}>
+            <Card className="flex flex-col md:flex-row justify-between md:space-x-8 rounded-xl border-4 border-indigo-800 p-14 space-y-4 md:space-y-0 md:p-8 lg:p-12">
+              <CardTitle>
+                <Avatar className="md:ml-0 w-24 h-24 md:w-28 md:h-28 lg:w-36 lg:h-36">
+                  <AvatarImage src={kid.image} />
                 </Avatar>
-              </div>
-            </CardTitle>
-            <CardDescription>
-              <div className="grid grid-cols-1 text-center space-y-2">
-                <div>
-                  <h1 className="text-xl text-black">Shaurya</h1>
-                  <p className="text-xs text-gray-500">Class 10</p>
+              </CardTitle>
+              <CardDescription className="md:flex md:flex-col md:items-center md:space-y-2">
+                <div className="text-center md:text-center space-y-4">
+                  <div className="">
+                    <h1 className="text-lg">{kid.name}</h1>
+                    <p className="text-xs text-gray-500">{kid.class}</p>
+                  </div>
+                  <h1 className="text-red-500">
+                    {kid.initialPercentage}% to{" "}
+                    <span className="text-green-400">{kid.finalPercentage}%</span>
+                  </h1>
+                  <div className="rounded-md p-1 bg-gray-100">
+                    <h1 className="text-sm text-black">{kid.duration}</h1>
+                  </div>
                 </div>
-                <h1 className="">
-                  <span className="text-red-500">47% </span> to{" "}
-                  <span className="text-xl text-green-400">85%</span>
-                </h1>
-                <div className="rounded-md p-1">
-                  <h1 className="text-sm text-black">In just 45 Days</h1>
-                </div>
-                <Button className="bg-[#2E23B8]">Connect</Button>
-              </div>
-            </CardDescription>
-          </Card>
-        </div>
-        <div>
-          <Card className="flex items-center justify-between rounded-xl border-4 border-indigo-800 p-6 md:p-12 w-[350px] h-[200px] md:w-[400px] md:h-[200px]">
-            <CardTitle>
-              <div className="flex items-center">
-                <Avatar className="w-28 h-28">
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                </Avatar>
-              </div>
-            </CardTitle>
-            <CardDescription>
-              <div className="grid grid-cols-1 text-center space-y-2">
-                <div>
-                  <h1 className="text-lg">Ujjwal</h1>
-                  <p className="text-xs text-gray-500">Class 10</p>
-                </div>
-                <h1 className="">
-                  <span className="text-red-500">40% </span> to{" "}
-                  <span className="text-xl text-green-400">83%</span>
-                </h1>
-                <div className="rounded-md p-1">
-                  <h1 className="text-sm text-black">In just 45 Days</h1>
-                </div>
-                <Button className="bg-[#2E23B8]">Connect</Button>
-              </div>
-            </CardDescription>
-          </Card>
-        </div>
-        <div>
-          <Card className="flex items-center justify-between rounded-xl border-4 border-indigo-800 p-6 md:p-12 w-[350px] h-[200px] md:w-[400px] md:h-[200px]">
-            <CardTitle>
-              <div className="flex items-center">
-                <Avatar className="w-28 h-28">
-                  <AvatarImage src="/Shruti.jpg" />
-                </Avatar>
-              </div>
-            </CardTitle>
-            <CardDescription>
-              <div className="grid grid-cols-1 text-center space-y-2">
-                <div>
-                  <h1 className="text-lg">Shruti</h1>
-                  <p className="text-xs text-gray-500">Class 10</p>
-                </div>
-                <h1 className="">
-                  <span className="text-red-500">25% </span> to{" "}
-                  <span className="text-xl text-green-400">65%</span>
-                </h1>
-                <div className="rounded-md p-1">
-                  <h1 className="text-sm text-black">In just 45 Days</h1>
-                </div>
-                <Button className="bg-[#2E23B8]">Connect</Button>
-              </div>
-            </CardDescription>
-          </Card>
-        </div>
-        <div>
-          <Card className="flex items-center justify-between rounded-xl border-4 border-indigo-800 p-6 md:p-12 w-[350px] h-[200px] md:w-[400px] md:h-[200px]">
-            <CardTitle>
-              <div className="flex items-center">
-                <Avatar className="w-28 h-28">
-                  <AvatarImage src="/Shaurya.jpg" />
-                </Avatar>
-              </div>
-            </CardTitle>
-            <CardDescription>
-              <div className="grid grid-cols-1 text-center space-y-2">
-                <div>
-                  <h1 className="text-lg">Shaurya</h1>
-                  <p className="text-xs text-gray-500">Class 10</p>
-                </div>
-                <h1 className="">
-                  <span className="text-red-500">47% </span> to{" "}
-                  <span className="text-xl text-green-400">85%</span>
-                </h1>
-                <div className="rounded-md p-1">
-                  <h1 className="text-sm text-black">In just 45 Days</h1>
-                </div>
-                <Button className="bg-[#2E23B8]">Connect</Button>
-              </div>
-            </CardDescription>
-          </Card>
-        </div>
-        <div>
-          <Card className="flex items-center justify-between rounded-xl border-4 border-indigo-800 p-6 md:p-12 w-[350px] h-[200px] md:w-[400px] md:h-[200px]">
-            <CardTitle>
-              <div className="flex items-center">
-                <Avatar className="w-28 h-28">
-                  <AvatarImage src="/Shaurya.jpg" />
-                </Avatar>
-              </div>
-            </CardTitle>
-            <CardDescription>
-              <div className="grid grid-cols-1 text-center space-y-2">
-                <div>
-                  <h1 className="text-lg">Shaurya</h1>
-                  <p className="text-xs text-gray-500">Class 10</p>
-                </div>
-                <h1 className="">
-                  <span className="text-red-500">47% </span> to{" "}
-                  <span className="text-xl text-green-400">85%</span>
-                </h1>
-                <div className="rounded-md p-1">
-                  <h1 className="text-sm text-black">In just 45 Days</h1>
-                </div>
-                <Button className="bg-[#2E23B8]">Connect</Button>
-              </div>
-            </CardDescription>
-          </Card>
-        </div>
-        <div>
-          <Card className="flex items-center justify-between rounded-xl border-4 border-indigo-800 p-6 md:p-12 w-[350px] h-[200px] md:w-[400px] md:h-[200px]">
-            <CardTitle>
-              <div className="flex items-center">
-                <Avatar className="w-28 h-28">
-                  <AvatarImage src="/Shaurya.jpg" />
-                </Avatar>
-              </div>
-            </CardTitle>
-            <CardDescription>
-              <div className="grid grid-cols-1 text-center space-y-2">
-                <div>
-                  <h1 className="text-lg">Shaurya</h1>
-                  <p className="text-xs text-gray-500">Class 10</p>
-                </div>
-                <h1 className="">
-                  <span className="text-red-500">47% </span> to{" "}
-                  <span className="text-xl text-green-400">85%</span>
-                </h1>
-                <div className="rounded-md p-1">
-                  <h1 className="text-sm text-black">In just 45 Days</h1>
-                </div>
-                <Button className="bg-[#2E23B8]">Connect</Button>
-              </div>
-            </CardDescription>
-          </Card>
-        </div>
+                <Button className="bg-indigo-800 mt-4 md:mt-auto">Connect</Button>
+              </CardDescription>
+            </Card>
+          </div>
+        ))}
       </div>
     </div>
   );
